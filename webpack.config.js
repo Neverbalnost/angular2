2b84d3a45a5e830cd284fdf3f04ec15f996ee87b
Yelena Kogodeeva <Yelena Kogodeeva>
2017-03-05 17:23:13 +0100

    remove unneded code

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,7 @@
 switch (process.env.NODE_ENV) {
   case 'prod':
   case 'production':
-    module.exports = require('./config/webpack.prod')({
-      env: 'production',
-
-      entry: {
-          app: './app.module.ts'
-      },
-
-      resolve: {
-        extensions: ['.js', '.ts']
-      },
-
-      plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-      ]
-    });
+    module.exports = require('./config/webpack.prod')({env: 'production'});
     break;
   case 'test':
   case 'testing':
