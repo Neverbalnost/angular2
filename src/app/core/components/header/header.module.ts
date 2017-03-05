1b0d0cb45de2aa0f6f6905d07161b9df4592f922
Yelena Kogodeeva <Yelena Kogodeeva>
2017-03-05 16:29:18 +0100

    Header done

--- src/app/core/components/header/header.module.ts
+++ src/app/core/components/header/header.module.ts
@@ -3,11 +3,13 @@ import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
 
 import { LogoComponent } from './logo/logo.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
 	declarations: [
 		HeaderComponent,
-		LogoComponent
+		LogoComponent,
+		LoginComponent
 	],
 	imports: [RouterModule],
 	exports: [HeaderComponent]
