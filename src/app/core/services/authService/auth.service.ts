a66bac49443df35e6f88e4600cc772ea578ec9b6
Elena Kogodeeva <Elena_Kogodeeva@epam.com>
2017-04-21 12:51:22 +0200

    extend http

--- src/app/core/services/authService/auth.service.ts
+++ src/app/core/services/authService/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { Response, Request, RequestOptions, RequestMethod, Http } from '@angular/http';
+import { Response, Request, RequestOptions, RequestMethod } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { HttpService } from './../httpService/http.service';
 
 import 'rxjs/add/operator/share';
 import 'rxjs/add/observable/throw';
@@ -19,7 +20,7 @@ export class AuthService {
 	private authUrl: string = 'auth/login';
 	private infoUrl: string = 'auth/userinfo';
 
-	constructor(private http: Http) {
+	constructor(private http: HttpService) {
 		this.IsAuthenticated = new Observable((observer) =>
 			this._observer = observer).share();
 	}
@@ -38,7 +39,6 @@ export class AuthService {
 			.subscribe((response) => {
 				localStorage.setItem('currUserToken', response.token);
 				this.userLoggedIn = true;
-				this.changeState(this.userLoggedIn);
 				this.GetUserInfo();
 			});
 	}
@@ -50,8 +50,8 @@ export class AuthService {
 	}
 
 	public GetUserInfo() {
-		const token = JSON.parse(localStorage.getItem('currUserToken'));
-		return this.http.post(this.infoUrl, token)
+		const token = localStorage.getItem('currUserToken');
+		this.http.post(this.infoUrl, token)
 			.catch((error: any) => {
 				alert(error._body);
 				return Observable.throw(error);
@@ -59,6 +59,7 @@ export class AuthService {
 			.map((response: Response) => response.json())
 			.subscribe((response) => {
 				this.changeUserInfo(response);
+				this.changeState(this.userLoggedIn);
 			});
 	}
 
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,3 +1,4 @@
 export * from './courseService/course.service';
 export * from './authService/auth.service';
 export * from './loaderService/loader.service';
+export * from './httpService/http.service';
