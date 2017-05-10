d0c57ebf3ca672b352a9691f99285c88ad5eb0e9
Elena Kogodeeva <Elena_Kogodeeva@epam.com>
2017-05-10 17:20:56 +0200

    404 page

--- src/app/pages/no-content/no-content.component.ts
+++ src/app/pages/no-content/no-content.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'no-content',
 	template: `
-    <div>
-      <h1>404: page missing</h1>
-    </div>
-  `
+		<div>
+			<h1>404: This URL is not heroic enough!</h1>
+			<img src="assets/img/404.jpg">
+		</div>
+	`
 })
 export class NoContentComponent {
 
